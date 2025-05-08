23ae2f264a3bbb4061918edafef919c546fa9884
etheriannn <rianziwalker@gmail.com>
2025-05-08 21:39:35 +0700

    style: fix formatting and add newline at end of OurToken contract

--- src/OurToken.sol
+++ src/OurToken.sol
@@ -3,10 +3,8 @@ pragma solidity ^0.8.18;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract OurToken is ERC20{
-
+contract OurToken is ERC20 {
     constructor(uint256 initialSupply) ERC20("OurToken", "OUR") {
         _mint(msg.sender, initialSupply);
     }
-
 }
